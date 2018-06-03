6a3371fed6f10560b531c7ee5a10a094f1757bef
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-06-03 18:25:55 +0200

    changed product order

--- assets/src/js/custom/custom.js
+++ assets/src/js/custom/custom.js
@@ -1,11 +1,18 @@
-window.onload = function(){
-	
+$(document).ready(function(){
+
+	// $('.wraper-link').hover(function(){
+     
+ //      $('.wraper-link:first-child .product-bg-overlay').css('opacity', '1');
+     
+	// }, function(){
+
+	// 	$('.product-bg-overlay').css('opacity', '0');
+	// });
+
+	// $('.product-bg-overlay').hover(function(){
+	// 	$(this).css('opacity', '1');
+	// 	$('.wraper-link').css('opacity','0');
 		
-	if( typeof Cookies.get("mojeIme") === "undefined"){
-		Cookies.set('mojeIme', 'Nemanja Mandic', { expires: 7 });
-       
-	}
-	
-	
-	 console.log(Cookies.get("mojeIme"));
-}
+	// });
+
+});
